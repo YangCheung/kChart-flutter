48dda1eb609c07cd8327d072338f345c9a552350
YangCheung <ssea.zh@gmail.com>
2018-11-21 14:30:07 +0800

    change listview for stock detail page

--- lib/page/KDetailBottomView.dart
+++ lib/page/KDetailBottomView.dart
@@ -19,24 +19,43 @@ class _KDetailBottomViewState extends State<KDetailBottomView> {
     return DefaultTabController(
       length: myTabs.length,
       initialIndex: 1,
-      child: Column(
-        children: <Widget>[
-          TabBar(
-            indicatorColor: Color(0xff1478F0),
-            indicatorWeight: 3,
-            unselectedLabelColor: Color(0xFF999999),
-            labelColor: Color(0xFF333333),
-            unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal, fontSize: 15.0),
-            labelStyle: TextStyle(fontWeight: FontWeight.w500, fontSize: 15.0),
-            labelPadding: EdgeInsets.symmetric(vertical: 5.0),
-            indicatorPadding: EdgeInsets.fromLTRB(2, 0, 2, 0),
-            indicatorSize: TabBarIndicatorSize.label,
-            tabs: myTabs.map((Tab tab) {
-              return Text(tab.text);
-            }).toList(),
-          )
-        ],
-      ),
+      child: Container(
+//        height: 100,
+        child: Column(
+          children: <Widget>[
+            TabBar(
+              indicatorColor: Color(0xff1478F0),
+              indicatorWeight: 3,
+              unselectedLabelColor: Color(0xFF999999),
+              labelColor: Color(0xFF333333),
+              unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal, fontSize: 15.0),
+              labelStyle: TextStyle(fontWeight: FontWeight.w500, fontSize: 15.0),
+              labelPadding: EdgeInsets.symmetric(vertical: 5.0),
+              indicatorPadding: EdgeInsets.fromLTRB(2, 0, 2, 0),
+              indicatorSize: TabBarIndicatorSize.label,
+              tabs: myTabs.map((Tab tab) {
+                return Text(tab.text);
+              }).toList(),
+            ),
+            Expanded(
+              child:TabBarView(
+                children: <Widget>[
+                  Container(
+                    height: 1000,
+
+                    child: Center(child: Text("1"),)
+                  ),
+                  Text("2"),
+                  Text("3"),
+                  Text("4"),
+                  Text("5"),
+                ],
+              ) ,
+            )
+
+          ],
+        )
+      ) ,
     );
   }
 }
